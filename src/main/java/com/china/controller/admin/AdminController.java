b17708ad12970b0fb8f29542a6c456230ff86774
lizengyy <737403523@qq.com>
2021-02-23 17:37:59 +0800

    create

--- src/main/java/com/china/controller/admin/AdminController.java
+++ src/main/java/com/china/controller/admin/AdminController.java
@@ -2,11 +2,15 @@ package com.china.controller.admin;
 
 import com.china.entity.admin.AdminEntity;
 import com.china.service.admin.AdminService;
+import com.china.utils.MapUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -27,7 +31,26 @@ public class AdminController {
         AdminEntity user = (AdminEntity)request.getSession().getAttribute(ticket);
         Map data = new HashMap<>();
         data.put("UserInfo", user);
+        data.put("state", user.getState());
+        data.put("PartName", user.getPartId());
+        data.put("Age", "xx年xx月xx天");
         return new ModelAndView("admin/admininfo", data);
+    }
+
+    @RequestMapping(value="/chgPwd", method= RequestMethod.POST)
+    @ResponseBody
+    public String chgPwd(HttpServletRequest request, @RequestParam Map param){
+        String opwd = MapUtil.safeStr("opwd", param);
+        String npwd = MapUtil.safeStr("npwd", param);
+        String cpwd = MapUtil.safeStr("cpwd", param);
+        String ticket = request.getAttribute("UserTicket")+"";
+        AdminEntity user = (AdminEntity)request.getSession().getAttribute(ticket);
+        if(user!=null && opwd.equals(user.getPwd())){
+            // TODO
+        }else{
+            return "输入旧密码不正确!";
+        }
+        return "1";
     }
 
 }
--- src/main/resources/static/js/admin/admin.js
+++ src/main/resources/static/js/admin/admin.js
@@ -1,9 +1,60 @@
 function chgPwd(){
-    $("#chgInputTr").show();
+    $(".chgInput").show();
     $("#chgBtnTd").hide();
     $("#submitChgTd").show();
 }
 
 function chgPwdSubmit(){
+    var opwd = $("#opwd").val();
+    var npwd = $("#npwd").val();
+    var cpwd = $("#cpwd").val();
+
+    if(opwd=='' || opwd==undefined){
+        $("#chgErrTd").text("请输入旧密码");
+        $("#chgErrTr").show();
+        return;
+    }
+    if(npwd=='' || npwd==undefined){
+        $("#chgErrTd").text("请输入新密码");
+        $("#chgErrTr").show();
+        return;
+    }
+    if(cpwd=='' || cpwd==undefined){
+        $("#chgErrTd").text("请确认新密码");
+        $("#chgErrTr").show();
+        return;
+    }
+
+    if(npwd != cpwd){
+        $("#chgErrTd").text("新密码输入不一致");
+        $("#chgErrTr").show();
+        return;
+    }
+
+    opwd = $.base64.encode(opwd);
+    npwd = $.base64.encode(npwd);
+    cpwd = $.base64.encode(cpwd);
+
+
+    $.ajax({
+        url: "/chgPwd",
+        data: {opwd: opwd, npwd: npwd, cpwd: cpwd},
+        type: "POST",
+        success: function(data) {
+            if(data=="1"){
+                $("#opwd").val("");
+                $("#npwd").val("");
+                $("#cpwd").val("");
+                $(".chgInput").hide();
+                $("#chgBtnTd").show();
+                $("#submitChgTd").hide();
+                $("#chgErrTr").hide();
+                popMsg("密码修改成功！");
+            }else{
+                $("#chgErrTd").text(data);
+                $("#chgErrTr").show();
+            }
+        }
+    });
 
 }
